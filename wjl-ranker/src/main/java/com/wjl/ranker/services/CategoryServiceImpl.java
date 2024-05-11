d6ac157c22b1f8dac1cb3b52a1bc9c16a438b7b1
wahjian.lin@gmail.com <wahjian.lin@gmail.com>
2024-05-11 16:27:32 -0400

    [MODIFY] FP-0002 Added logging to services.

--- wjl-ranker/src/main/java/com/wjl/ranker/services/CategoryServiceImpl.java
+++ wjl-ranker/src/main/java/com/wjl/ranker/services/CategoryServiceImpl.java
@@ -1,16 +1,20 @@
 package com.wjl.ranker.services;
 
+import com.wjl.ranker.Constants;
 import com.wjl.ranker.entities.Category;
 import com.wjl.ranker.exception.GeneralException;
 import com.wjl.ranker.repositories.CategoryRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class CategoryServiceImpl implements CategoryService {
-    // TODO create validator that validates the CategoriesDTOs
+    // TODO create validator
+    public static final String ENTITY = "CATEGORY";
 
     private final CategoryRepo categoryRepo;
 
@@ -26,15 +30,20 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category getCategoryById(Long id) {
-        return categoryRepo.findById(id).orElseThrow(() -> new GeneralException("Category not found"));
+        return categoryRepo.findById(id).orElseThrow(() -> {
+            log.error(String.format(Constants.LOG_FAILED_FIND, ENTITY, id));
+            return new GeneralException(String.format(Constants.EXCEPTION_GENERAL_NOT_FOUND, ENTITY));
+        });
     }
 
     @Override
     public Category createCategory(Category categoryEntity) {
         try {
+            log.info((String.format(Constants.LOG_ATTEMPTING_TO_SAVE, ENTITY, categoryEntity.getId(), categoryEntity)));
             return categoryRepo.save(categoryEntity);
         } catch (Exception e) {
-            throw new GeneralException("Category failed to Save");
+            log.error((String.format(Constants.LOG_FAILED_SAVE, ENTITY, categoryEntity.getId(), categoryEntity)));
+            throw new GeneralException(String.format(Constants.EXCEPTION_GENERAL_FAILED_SAVE, ENTITY));
         }
     }
 
@@ -42,20 +51,25 @@ public class CategoryServiceImpl implements CategoryService {
     public Category updateCategory(Category categoryEntity) {
         Category category = getCategoryById(categoryEntity.getId());
         try {
+            log.info((String.format(Constants.LOG_ATTEMPTING_TO_UPDATE, ENTITY, categoryEntity.getId(), categoryEntity)));
+
             category.setName(categoryEntity.getName());
             category.setDescription(categoryEntity.getDescription());
             return categoryRepo.save(category);
         } catch (Exception e) {
-            throw new GeneralException("Category failed to update");
+            log.error((String.format(Constants.LOG_FAILED_UPDATE, ENTITY, categoryEntity.getId(), categoryEntity)));
+            throw new GeneralException(String.format(Constants.EXCEPTION_GENERAL_FAILED_UPDATE, ENTITY));
         }
     }
 
     @Override
     public void deleteCategory(long id) {
+        log.info(String.format(Constants.LOG_ATTEMPTING_TO_DELETE, ENTITY, id));
         if (!categoryRepo.existsById(id)) {
-            throw new GeneralException("Category " + id + " does not exist");
-        } else {
-            categoryRepo.deleteById(id);
+            log.error(String.format(Constants.LOG_FAILED_DELETE, ENTITY, id));
+            throw new GeneralException(String.format(Constants.EXCEPTION_GENERAL_FAILED_DELETE, ENTITY));
         }
+        categoryRepo.deleteById(id);
+        log.info(String.format(Constants.LOG_SUCCESSFUL_DELETE, ENTITY, id));
     }
 }
